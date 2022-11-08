d0ae2ae82e1d1a7f407ff15886fa2918cc214e99
LobatoLobato <felipelobato.rj@gmail.com>
2022-11-07 21:47:51 -0300

    Teste

--- src/Models/Modulo.ts
+++ src/Models/Modulo.ts
@@ -1,16 +1,13 @@
 export abstract class Modulo {
-
-    public static context: AudioContext
-    public static listener: AudioListener
-    protected maxValue: number = 1
-    protected minValue: number = 1
-    public abstract node: AudioNode
-    protected currentTime(): number {
-        return 0
-    }
-    public abstract connect(destination: AudioNode | AudioParam): void
-    public abstract start(): void
-    public abstract stop(): void
-
-
+  public static context: AudioContext = new AudioContext();
+  public static listener: AudioListener = this.context.listener;
+  protected maxValue: number = 1;
+  protected minValue: number = 1;
+  public abstract node: AudioNode;
+  protected currentTime(): number {
+    return Modulo.context.currentTime;
+  }
+  public abstract connect(destination: AudioNode | AudioParam): void;
+  public abstract start(): void;
+  public abstract stop(): void;
 }
